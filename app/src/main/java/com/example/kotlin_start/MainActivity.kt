f7bb0bc9cefe2c1bfcde6e09866ee9a55cfd78d8
korvin777kds@mail.ru <korvin777kds@mail.ru>
2021-06-14 03:22:36 +0300

    start homework kotlin

--- app/src/main/java/com/example/kotlin_start/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_start/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.kotlin_start
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.kotlin_start.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root);
+        binding.titleApplication.setOnClickListener {
+            Toast.makeText(applicationContext, "Title Application Click", Toast.LENGTH_SHORT).show()
+        }
     }
 }
